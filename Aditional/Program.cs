e6a482c5b5738e06b8906d7e85df0af8cf989d2b
Timur <tumyp.paxumob@gmail.com>
2022-07-20 02:03:12 +0500

    Added files

---
+++ Aditional/Program.cs
@@ -0,0 +1,14 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string a = string.Empty;
+        int b = 19;
+
+        for (; b > 0; b /= 2)
+            a = b % 2 + a;
+        Console.WriteLine(a);
+    }
+}
